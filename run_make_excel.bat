83637a2118a2738df97ab4907218ce0cb8238560
Andrey Putilov <Andrey.Putilov@materialplus.io>
2025-09-27 21:15:37 -0700

    major updates to mdm-report-excel-py

--- run_make_excel.bat
+++ run_make_excel.bat
@@ -58,7 +58,7 @@ ECHO -
 ECHO 1. read MDD
 ECHO read from: %MDD_FILE%
 ECHO write to: .json
-python dist/mdmoverlayhelper_bundle.py --program read_mdd --mdd "%MDD_FILE%" --config-features translations --config-section fields --config-contexts Analysis,Question
+python dist/mdmoverlayhelper_bundle.py --program read_mdd --mdd "%MDD_FILE%" --config-features translations --config-section shared_lists,fields,pages --config-contexts Question --config-sharedlists-listcats stepover
 if %ERRORLEVEL% NEQ 0 ( echo ERROR: Failure && pause && goto CLEANUP && exit /b %errorlevel% )
 
 IF %CONFIG_PRODUCE_HTML_MDD% (
